515f111a9f905b2a29858a3121d958c8a564ec8b
lovelysanthoshp-cyber <lovelysanthoshp@gmail.com>
2026-02-06 14:40:28 +0530

    Add functions to calculate GCD and common factors

---
+++ NO OF COMMON FACTORS.c
@@ -0,0 +1,21 @@
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int commonFactors(int a, int b) {
+    int g = gcd(a, b);
+    int count = 0;
+
+    for (int i = 1; i <= g; i++) {
+        if (g % i == 0) {
+            count++;
+        }
+    }
+
+    return count;
+}
