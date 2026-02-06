16c0c94b1d5cae0844818066fbc8e86b146993c5
lovelysanthoshp-cyber <lovelysanthoshp@gmail.com>
2026-02-06 15:22:57 +0530

    Add rectangle pattern printing program
    Implement a program to print a rectangle pattern using asterisks.

---
+++ Rectangle Pattern.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int rows,columns;
+    
+    scanf("%d %d",&rows,&columns);
+    for(int i=1;i<=rows;i++)
+    {
+        for(int j=1;j<=columns;j++)
+        {
+
+               printf("*");
+            
+             }
+         printf("\n");
+        }
+   
+    
+    
+    return 0;
+    }
