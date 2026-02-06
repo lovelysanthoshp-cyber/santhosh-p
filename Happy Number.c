c39d3cfb766c120c2b17a7082960ea6a58cb9cee
lovelysanthoshp-cyber <lovelysanthoshp@gmail.com>
2026-02-06 15:20:08 +0530

    Implement Happy Number algorithm in C

---
+++ Happy Number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int sumOfSquares(int n) {
+    int sum = 0;
+    while (n > 0) {
+        int digit = n % 10;
+        sum += digit * digit;
+        n /= 10;
+    }
+    return sum;
+}
+
+bool isHappy(int n) {
+    int slow = n;
+    int fast = n;
+
+    do {
+        slow = sumOfSquares(slow);
+        fast = sumOfSquares(sumOfSquares(fast));
+    } while (slow != fast);
+
+    return slow == 1;
+}
+}
