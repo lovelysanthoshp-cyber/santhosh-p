5833319d4ee0e2c10a5e99e1a2c53f011c90c7b3
lovelysanthoshp-cyber <lovelysanthoshp@gmail.com>
2026-02-06 12:39:38 +0530

    Implement triangle type detection in C

---
+++ Triangle Type Detector.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+       int a,b,c;
+       scanf("%d%d%d",&a,&b,&c);
+       if(a==b&&a==c){
+           printf("Equilateral Triangle");
+       }
+          else if (a!= b&&c)
+          printf("Not a valid triangle");
+    return 0;
+}
